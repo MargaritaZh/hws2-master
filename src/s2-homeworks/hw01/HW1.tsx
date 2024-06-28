580116a10d29580ee64bd3c2399b046a6efbd8bc
MargaritaZh <margaritazhitkevich@gmail.com>
2024-06-29 00:39:46 +0300

    homework-1-4

--- src/s2-homeworks/hw01/HW1.tsx
+++ src/s2-homeworks/hw01/HW1.tsx
@@ -14,7 +14,24 @@ import avatar from './avatar.png'
 * */
 
 // нужно создать правильный тип вместо any
-export type MessageType = any
+// export type MessageType = any
+
+
+export type MessageType = {
+    id: number;
+    user: UserType;
+    message: MessageInfoProps;
+};
+
+type UserType = {
+    avatar: string;
+    name: string;
+}
+
+type MessageInfoProps = {
+    text: string;
+    time: string;
+}
 
 // структуру объекта не менять
 export const message0: MessageType = {
@@ -47,12 +64,12 @@ const HW1 = () => {
             <div className={s2.hw}>
                 {/*проверка отображения (не менять)*/}
                 <div>
-                    <Message message={message0} />
-                    <FriendMessage message={friendMessage0} />
+                    <Message message={message0}/>
+                    <FriendMessage message={friendMessage0}/>
                 </div>
 
                 {/*для автоматической проверки дз (не менять)*/}
-                <MessageSender M={Message} />
+                <MessageSender M={Message}/>
             </div>
         </div>
     )
